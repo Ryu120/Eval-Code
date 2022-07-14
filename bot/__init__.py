f9abe65a6a18484fb22c40a8402ec10281a10b01
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-07-14 12:41:12 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -21,10 +21,16 @@ start_time = time.time()
 
 name = 'bot'
 
-# Read from config file
-config_file = f"{name}.ini"
-config = ConfigParser()
-config.read(config_file)
+"""
+API_ID = int(os.environ.get("API_ID", None))
+API_HASH = os.environ.get("API_HASH", None)
+TOKEN = os.environ.get("TOKEN", None)
+BOTUSERNAME = os.environ.get("BOTUSERNAME", None) 
+"""
+API_ID = "19733238"
+API_HASH = "fe955e75a36ec386ae9ded33061fbd98"
+TOKEN = "5566162550:AAFxuB_il3Zfn7fhhRHMZr_CIBRs_aQly2M"
+BOTUSERNAME = "RyuEvalBot"
 
 # Extra details
 __version__ = '0.0.1'
