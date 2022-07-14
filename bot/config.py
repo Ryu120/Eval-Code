96784e327bee80ad741ae7931436ff2c69896824
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-07-14 12:30:57 +0530

    Gay

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,7 @@ import re
 from pathlib import Path
 
 class Config:
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  TOKEN = os.environ.get("TOKEN", "")
-  BOTUSERNAME = os.environ.get("BOTUSERNAME", "")
+  API_ID = int(os.environ.get("API_ID", None))
+  API_HASH = os.environ.get("API_HASH", None)
+  TOKEN = os.environ.get("TOKEN", None)
+  BOTUSERNAME = os.environ.get("BOTUSERNAME", None)
